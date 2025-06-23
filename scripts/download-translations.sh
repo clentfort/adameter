31f81d31a6dffa2cc75be3b0c1f24ab286cd3221
Christian Lentfort <1284808+clentfort@users.noreply.github.com>
2025-06-23 20:49:42 +0200

    Update download-translations.sh

--- scripts/download-translations.sh
+++ scripts/download-translations.sh
@@ -9,10 +9,6 @@ BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 pnpm run fbtee:manifest
 pnpm run fbtee:collect
 
-# Linking the file to match the expected name as the crowdin CLI does not support
-# files with a leading dot
-ln -s .source_strings.json source_strings.json
 pnpm exec crowdin pull -b ${BRANCH_NAME} --plain translations
-rm source_strings.json
 
 pnpm run fbtee:translate
