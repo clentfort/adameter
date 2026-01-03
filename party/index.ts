4ff49e59a3b4615bb5892ad8147eaf0861a3ba0c
Christian Lentfort <1284808+clentfort@users.noreply.github.com>
2026-01-03 11:56:35 +0100

    bye bye yjs

--- party/index.ts
+++ party/index.ts
@@ -1,12 +1,3 @@
-import type { Connection, Room, Server, Worker } from 'partykit/server';
-import { onConnect } from 'y-partykit';
+import {TinyBasePartyKitServer} from 'tinybase/persisters/persister-partykit-server';
 
-export default class ServerImpl implements Server {
-	constructor(readonly room: Room) {}
-
-	onConnect(conn: Connection) {
-		return onConnect(conn, this.room, { persist: { mode: 'snapshot' } });
-	}
-}
-
-ServerImpl satisfies Worker;
+export default class extends TinyBasePartyKitServer {}
--- src/contexts/data-synchronization-context.tsx
+++ src/contexts/data-synchronization-context.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import { createContext, useContext, useEffect, useState } from 'react';
-import YPartyKitProvider from 'y-partykit/provider';
-import { Doc as YjsDoc } from 'yjs';
-import { yjsContext } from './yjs-context';
+import { createContext } from 'react';
 
 interface DataSynchronizationContextProps {
 	room: string | undefined;
@@ -23,47 +20,5 @@ interface DataSynchronizationProviderProps {
 export function DataSynchronizationProvider({
 	children,
 }: DataSynchronizationProviderProps) {
-	const [room, setRoom] = useState<string | undefined>(undefined);
-	const { doc } = useContext(yjsContext);
-	useEffect(() => {
-		const room = localStorage.getItem('room');
-		if (room) {
-			setRoom(room);
-		}
-	}, []);
-	useYPartykitSync(room, doc);
-	useEffect(() => {
-		if (!room) {
-			return;
-		}
-		localStorage.setItem('room', room);
-	}, [room]);
-
-	return (
-		<DataSynchronizationContext.Provider value={{ room, setRoom }}>
-			{children}
-		</DataSynchronizationContext.Provider>
-	);
-}
-
-/**
- * Sets up synchronization of Yjs document via PartyKit
- */
-function useYPartykitSync(room: string | undefined, doc: YjsDoc) {
-	useEffect(() => {
-		if (!room) {
-			return;
-		}
-
-		const provider = new YPartyKitProvider(
-			'https://adameter-party.clentfort.partykit.dev',
-			room,
-			doc,
-			{ connect: true },
-		);
-
-		return () => {
-			provider.destroy();
-		};
-	}, [room, doc]);
+	return children;
 }
